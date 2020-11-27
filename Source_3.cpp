137e1dc6b087a166045f967928ce35f297e0ce46
CrAzzY-WoRLd <dalinatalisman@gmail.com>
2020-11-27 23:02:17 +0200

    Source_3.cpp

---
+++ Source_3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include "windows.h"
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int k;
+
+	cout << "Введіть оцінку (1-4): ";
+	cin >> k;
+
+	switch (k)
+	{
+	case 1: cout << "Один"; break;
+	case 2: cout << "Два"; break;
+	case 3: cout << "Три"; break;
+	case 4: cout << "Чотири"; break;
+	
+	}
+}
